1ef8966594e40b7c0e423b618df26653e5a5af28
DESKTOP-FO8AFN4\Vitaliy <shadespiritenator@gmail.com>
2017-07-12 02:00:21 +0300

    start

---
+++ awesome-charts/src/components/Nev.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="nev">
+    <div class="container">
+      <div class="Chart__list">
+        <div class="Chart">
+          <h2>Linechart</h2>
+            <h1>{{ msg }}</h1>
+          <!--line-example></line-example-->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nev',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
